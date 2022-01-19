007fe8c69725dccccde8710fee7b578d3b1c0aea
he55 <40764272+he55@users.noreply.github.com>
2022-01-19 14:07:20 +0800

    perf

--- ASSWinFormsApp1/ASSWinFormsApp1/Window1.xaml.cs
+++ ASSWinFormsApp1/ASSWinFormsApp1/Window1.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ASSWinFormsApp1
 {
@@ -28,8 +18,16 @@ namespace ASSWinFormsApp1
         {
             set
             {
-                Uri uri = new Uri(value);
-                image1.Source = new BitmapImage(uri);
+                GC.Collect();
+
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource = new Uri(value);
+                bitmapImage.DecodePixelHeight = (int)this.Height;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                image1.Source = bitmapImage;
             }
         }
     }
