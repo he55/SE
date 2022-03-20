89765ce0d24b053459f4da680658d58d8f272ca1
he55 <40764272+he55@users.noreply.github.com>
2022-03-20 17:07:38 +0800

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ASSWinFormsApp1
@@ -9,15 +10,40 @@ namespace ASSWinFormsApp1
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            try
+            {
+                string filePath = Helper.GetPathForAppFolder("lock");
+                File.Open(filePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+            }
+            catch
+            {
+                MessageBox.Show("当前已经有一个实例在运行。", Constant.ProjectName);
+                return;
+            }
+
 #if NET5_0_OR_GREATER
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
 #endif
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            if (args.Length != 0 && args[0] == Constant.Cmd)
+            {
+                MainForm mainForm = new MainForm();
+                mainForm.Opacity = 0;
+                mainForm.Show();
+
+                mainForm.Hide();
+                mainForm.Opacity = 1;
+
+                Application.Run();
+            }
+            else
+            {
+                Application.Run(new MainForm());
+            }
         }
     }
 }
